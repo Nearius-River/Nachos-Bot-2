a155adabb8c6b6964ebdbb3b9b043072e2392f85
Nearius River <minecom4321@hotmail.com>
2020-05-04 18:48:24 -0300

    Updated to use #updateLog instead of #makeLog

--- events/resume.js
+++ events/resume.js
@@ -1,4 +1,4 @@
 module.exports = bot => {
     console.log(`BOT | O bot foi reconectado ao WebSocket com sucesso.`.ready);
-    bot.makeLog('Bot reconectado com sucesso!');
+    bot.updateLog('Bot reconectado com sucesso!');
 };
